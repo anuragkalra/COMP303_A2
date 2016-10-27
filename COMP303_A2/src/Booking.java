5be283a56c433c4926a6714610db58e5d262a651
anuragkalra <anuragkalra@outlook.com>
2016-10-26 20:20:22 -0400

    built main method

--- COMP303_A2/src/Booking.java
+++ COMP303_A2/src/Booking.java
@@ -8,6 +8,17 @@ import java.util.*;
 public class Booking {
 	public static void main(String[] args){
 		Airplane a = new Airplane();
+		
+		Runnable r1 = new GUISelector();
+		Runnable r2 = new RandomSelector();
+		Runnable r3 = new RandomSelector();
+		
+		Thread t1 = new Thread(r1);
+		Thread t2 = new Thread(r2);
+		Thread t3 = new Thread(r3);
+		
+		t1.start();
+		t2.start();
+		t3.start();
 	}
-
 }
